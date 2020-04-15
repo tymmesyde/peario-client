e33acf59b771a54d5ee4e250bc7edce1895d7d0b
unknown <tymmesyde@gmail.com>
2020-04-15 10:32:01 +0200

    :children_crossing: Update UI/UX of Search view

--- src/views/search/Search.vue
+++ src/views/search/Search.vue
@@ -3,17 +3,25 @@
         <Header></Header>
 
         <div class="inner">
-            <h1>
-                <ion-icon name="search"></ion-icon>
-                Search
-            </h1>
-            <h3>Find a movie to watch with your 🍐 !</h3>
+            <div class="title">
+                <h1>
+                    <ion-icon name="search"></ion-icon>
+                    Search
+                </h1>
+                <h3>Find a movie to watch with your 🍐 !</h3>
+            </div>
 
-            <input type="text" v-model="search">
+            <input type="text" v-model="search" placeholder="Parasite, Fight Club, ...">
             <ul class="list">
                 <li v-for="result in results" :key="result.imdb_id" @click="$router.push({ name: 'stream', params: { type: 'movie', id: result.imdb_id } })">
-                    <img :src="result.poster" alt="poster">
-                    {{ result.name }}
+                    <div class="poster">
+                        <ion-icon name="image-outline" v-if="!result.poster"></ion-icon>
+                        <img :src="result.poster" alt="poster" v-if="result.poster">
+                    </div>
+                    <div class="info">
+                        <p class="name">{{ result.name }}</p>
+                        <p class="year">{{ result.releaseInfo }}</p>
+                    </div>
                 </li>
             </ul>
         </div>
@@ -36,13 +44,17 @@ export default {
     data() {
         return {
             search: '',
-            results: []
+            results: [],
+            debouncer: null
         }
     },
     watch: {
-        async search(value) {
-            if (value.length) this.results = await StremioService.searchMovies(value);
-            else this.results = [];
+        search(value) {
+            clearTimeout(this.debouncer);
+            this.debouncer = setTimeout(async () => {
+                if (value.length) this.results = await StremioService.searchMovies(value);
+                else this.results = [];
+            }, 500);
         }
     }
 }
@@ -50,19 +62,76 @@ export default {
 
 <style lang="scss" scoped>
 #search {
+    .title {
+        text-align: center;
+        margin-bottom: 4.5vh;
+
+        h1 {
+            margin-bottom: 0.5vh;
+            margin-left: -4vh;
+
+            ion-icon {
+                font-size: 7vh;
+                vertical-align: -0.75vh;
+            }
+        }
+    }
+
     .list {
-        max-height: 20vh;
+        max-height: 35vh;
+        width: 55vh;
         overflow: hidden;
         overflow-y: auto;
+        margin: auto;
+        margin-top: 2vh;
+        border-radius: 2vh;
+        background-color: rgba(0, 0, 0, 0.2);
+        box-shadow: 0 0.5vh 1vh rgba(0, 0, 0, 0.1);
 
         li {
             display: grid;
             grid-template-columns: 9vh auto;
-            grid-column-gap: 1vh;
+            grid-column-gap: 1.5vh;
+            padding: 2vh;
             cursor: pointer;
+            transition: all 0.1s ease-in-out;
+            transition-delay: 0.05s;
 
-            img {
-                width: 100%;
+            .poster {
+                display: grid;
+                align-content: center;
+                height: 13vh;
+                overflow: hidden;
+                background-color: rgba(255, 255, 255, 0.1);
+                border-radius: 0.5vh;
+
+                ion-icon {
+                    font-size: 5vh;
+                    opacity: 0.8;
+                    margin: auto;
+                }
+
+                img {
+                    width: 100%;
+                }
+            }
+            
+            .info {
+                margin-top: 1vh;
+
+                .name {
+                    font-size: 2.2vh;
+                    margin-bottom: 0.5vh;
+                }
+
+                .year {
+                    font-size: 1.75vh;
+                    font-style: italic;
+                }
+            }
+
+            &:hover {
+                background-color: rgba(0, 0, 0, 0.2);
             }
         }
     }
