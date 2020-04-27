b4e0671fe31ed338bf20357db68f60d492dd43ec
Tim <tymmesyde@gmail.com>
2020-04-28 01:00:34 +0200

    :globe_with_meridians: Add Turkish and Hindi locales

--- src/common/locales/index.js
+++ src/common/locales/index.js
@@ -1,4 +1,6 @@
 const en = require('./en.json');
 const fr = require('./fr.json');
+const tr = require('./tr.json');
+const hi = require('./hi.json');
 
-module.exports = { en, fr };
+module.exports = { en, fr, tr, hi };
