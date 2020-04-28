41c34da35c8b0f0eb16cbfe415dc2faff5d83dde
Tim <tymmesyde@gmail.com>
2020-04-28 21:42:49 +0200

    :globe_with_meridians: Add Portugese to locales

--- src/common/locales/index.js
+++ src/common/locales/index.js
@@ -2,5 +2,6 @@ const en = require('./en.json');
 const fr = require('./fr.json');
 const tr = require('./tr.json');
 const hi = require('./hi.json');
+const pt = require('./pt.json');
 
-module.exports = { en, fr, tr, hi };
+module.exports = { en, fr, tr, hi, pt };
