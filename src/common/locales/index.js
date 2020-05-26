ceb0e44bf07ca7c37223cc384cfe2a27ecb1b9e9
Tim <tymmesyde@gmail.com>
2020-05-26 11:04:15 +0200

    :globe_with_meridians: Add Spanish to locales

--- src/common/locales/index.js
+++ src/common/locales/index.js
@@ -3,5 +3,6 @@ const fr = require('./fr.json');
 const tr = require('./tr.json');
 const hi = require('./hi.json');
 const pt = require('./pt.json');
+const es = require('./es.json');
 
-module.exports = { en, fr, tr, hi, pt };
+module.exports = { en, fr, tr, hi, pt, es };
