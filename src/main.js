f3b24f45a6d7e5513a185cb818604fda0bb0ba63
Tim <tymmesyde@gmail.com>
2021-02-19 21:42:20 +0100

    refactor: remove remote scripts for ionicons

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import { createApp } from 'vue';
 import Toaster from "@meforma/vue-toaster";
 import router from './router';
+import Ionicons from './plugins/ionicons';
 import App from './App.vue';
 
 createApp(App)
     .use(router)
+    .use(Ionicons)
     .use(Toaster, {
         position: 'bottom',
         duration: 3000
