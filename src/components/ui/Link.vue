cb9c7f3eafafd9e9c92cf40d57d8a8858b43d222
Tim <tymmesyde@gmail.com>
2021-07-14 09:37:10 +0200

    refactor(Home): add link to stremio website

---
+++ src/components/ui/Link.vue
@@ -0,0 +1,30 @@
+<template>
+    <span class="link" @click="open()">
+        <slot></slot>
+    </span>
+</template>
+
+<script>
+export default {
+    props: {
+        href: String
+    },
+    methods: {
+        open() {
+            window.open(this.href);
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.link {
+    color: $accent-color;
+    cursor: pointer;
+    transition: all 0.1s ease-in-out;
+
+    &:hover {
+        box-shadow: 0 2px 0 0 $accent-color;
+    }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,11 @@
     <div class="home">
         <div class="title">
             <Title translate="views.home.title"/>
-            <Title type="tertiary" translate="views.home.sub"/>
+            <Title type="tertiary">
+                <i18n-t keypath="views.home.sub" tag="span">
+                    <Link href="https://stremio.com">Stremio</Link>
+                </i18n-t>
+            </Title>
         </div>
 
         <Button large icon="play" @click="goToSearch()">
@@ -13,12 +17,14 @@
 
 <script>
 import Title from '@/components/ui/Title.vue';
+import Link from '@/components/ui/Link.vue';
 import Button from '@/components/ui/Button.vue';
 
 export default {
     name: 'Home',
     components: {
         Title,
+        Link,
         Button
     },
     methods: {
