7b4b7a8f917aab377fbb888fb6471df47dfe4927
Tim <tymmesyde@gmail.com>
2022-05-01 13:28:41 +0200

    fix(addon.service.js): don't filter idPrefixes for subtitles addons

--- src/services/addon.service.js
+++ src/services/addon.service.js
@@ -7,12 +7,13 @@ const AddonService = {
 
         addons.map(descriptor => col.add(AddonClient.fromDescriptor(descriptor)));
 
-        const filter = (manifest, resource) => {
+        const filter = (manifest, resource, prefix = null) => {
             const { id, idPrefixes, resources, types } = manifest;
-            return id !== 'community.peario' && (idPrefixes && idPrefixes.includes('tt') && resources && resources.includes(resource) && (types.includes('movie') || types.includes('series')) || resources.find(({ name, idPrefixes, types }) => name === resource && idPrefixes && idPrefixes.includes('tt') && types && (types.includes('movie') || types.includes('series'))));
+            return id !== 'community.peario' && ((prefix ? idPrefixes && idPrefixes.includes(prefix) : true) && resources && resources.includes(resource) && (types.includes('movie') || types.includes('series')))
+            || (resources.find(({ name, idPrefixes, types }) => name === resource && (prefix ? idPrefixes && idPrefixes.includes(prefix) : true) && types && (types.includes('movie') || types.includes('series'))));
         };
 
-        const streams = col.getAddons().filter(({ manifest }) => filter(manifest, 'stream'));
+        const streams = col.getAddons().filter(({ manifest }) => filter(manifest, 'stream', 'tt'));
         const subtitles = col.getAddons().filter(({ manifest }) => filter(manifest, 'subtitles'));
 
         return {
